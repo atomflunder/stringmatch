d42e9acd0c113a553f5b6f4a88ea4e69d7943a93
atomflunder <80397293+atomflunder@users.noreply.github.com>
2022-04-25 16:39:59 +0200

    made some changes to prepare going public

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 from setuptools import find_packages, setup
 
+import searchlib
+
+required_packages = []
+with open("requirements.txt", "r") as f:
+    required_packages = f.read().split()
+
+readme = ""
+with open("README.md", "r") as f:
+    readme = f.read()
+
 setup(
     name="searchlib",
-    packages=find_packages(include=["searchlib"]),
-    version="0.2.0",
-    description="A library to match and compare strings.",
     author="atomflunder",
+    url="https://github.com/atomflunder/searchlib",
     license="MIT",
-    install_requires=[
-        "Levenshtein",
-        "Unidecode",
-    ],
+    description="A library to match and compare strings.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["searchlib"]),
+    version=searchlib.__version__,
+    install_requires=required_packages,
+    python_requires=">=3.9",
     tests_require=["pytest"],
     test_suite="tests",
 )
