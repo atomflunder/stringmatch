2bc876f7171d9eb209dbdcce00bd7f826ea345b6
atomflunder <80397293+atomflunder@users.noreply.github.com>
2022-04-27 23:17:21 +0200

    v0.6.0 - Return type of distance funcs optional

--- stringmatch/distance.py
+++ stringmatch/distance.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 import Levenshtein
 
 
 class Distance:
     """Contains functions for calculating the levenshtein distance between strings."""
 
-    def distance(self, string1: str, string2: str) -> int:
+    def distance(self, string1: str, string2: str) -> Optional[int]:
         """Returns the levenshtein distance between two strings.
 
         Parameters
@@ -16,12 +18,12 @@ class Distance:
 
         Returns
         -------
-        int
+        Optional[int]
             The levenshtein distance between the two strings.
         """
-        return Levenshtein.distance(string1, string2)
+        return Levenshtein.distance(string1, string2) if string1 and string2 else None
 
-    def distance_list(self, string: str, string_list: list[str]) -> list[int]:
+    def distance_list(self, string: str, string_list: list[str]) -> list[Optional[int]]:
         """Returns the levenshtein distance for a string and a list of strings.
 
         Parameters
@@ -33,7 +35,7 @@ class Distance:
 
         Returns
         -------
-        list[int]
+        list[Optional[int]]
             The levenshtein distances between the two strings.
         """
         return [self.distance(string, s) for s in string_list]
--- tests/test_distance.py
+++ tests/test_distance.py
@@ -14,5 +14,6 @@ def test_distance():
 def test_distance_list():
     assert Distance().distance_list("kitten", ["sitting", "kitten"]) == [3, 0]
     assert Distance().distance_list(
-        "stringmatch", ["strmtc", "string", "match", "matchstring"]
-    ) == [5, 5, 6, 10]
+        "stringmatch", ["strmtc", "string", "match", "matchstring", ""]
+    ) == [5, 5, 6, 10, None]
+    assert Distance().distance("kitten", "") is None
