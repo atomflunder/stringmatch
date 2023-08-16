cc211a9a8e87ea1f02faaed9e4a9b1a05319cb99
atomflunder <80397293+atomflunder@users.noreply.github.com>
2023-08-16 21:38:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("stringmatch/__init__.py", encoding="utf-8") as f:
         if line.startswith("__version__"):
             version = line.split("=")[1].strip().strip('"')
 
-required_packages = ["rapidfuzz==3.2.0", "unidecode==1.3.6", "mypy==1.5.0"]
+required_packages = ["rapidfuzz==3.2.0", "unidecode==1.3.6", "mypy==1.5.1"]
 
 readme = ""
 with open("README.md", "r", encoding="utf-8") as f:
