53fb6ae140de3cbf87e7ef3751501f95aadcff48
atomflunder <80397293+atomflunder@users.noreply.github.com>
2026-02-19 21:06:36 +0100

    Bump version

--- stringmatch/__init__.py
+++ stringmatch/__init__.py
@@ -10,7 +10,7 @@ from stringmatch.scorer import (
 from stringmatch.strings import Strings
 
 __title__ = "stringmatch"
-__version__ = "0.14.7"
+__version__ = "0.14.8"
 __all__ = (
     "Distance",
     "Match",
